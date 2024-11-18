34e9a62402f91a2914de1784e66ceb738f42cd82
innocent mwea <name.infx@gmail.com>
2024-11-18 14:34:35 +0300

    Update admin.py

--- administration/admin.py
+++ administration/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
 from .models import Designation
+from django.apps import AppConfig
+from django.contrib.auth.models import User
+
 
 admin.site.register(Designation)
+# administration/apps.py
+
+class AdministrationConfig(AppConfig):
+    name = 'administration'
+
+    def ready(self):
+        # Check and create superuser if not exists
+        from django.db.models.signals import post_migrate
+        post_migrate.connect(self.create_superuser, sender=self)
+
+    def create_superuser(self, sender, **kwargs):
+        username = 'inno'
+        email = 'admin@domain.com'
+        password = 'p@zzword'
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
